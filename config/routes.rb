830dce4a3e974534f0312f24879579678aaff969
Yuri Ramocan <yramocan@umich.edu>
2015-07-08 03:17:13 +0000

    Improved the about page link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   
-  root 'pages#home'
-  
   get '/about' => 'pages#about'
+  root 'pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
