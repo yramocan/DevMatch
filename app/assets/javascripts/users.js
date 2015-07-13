d2690ba7c9845e7d6559080182d95c3c3b1f2d59
Yuri Ramocan <yramocan@umich.edu>
2015-07-13 22:44:09 +0000

    Fixed stripe integration error and removed contacts.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
-  
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-  
   // Watch for a form submission:
   $("#form-submit-btn").click(function(event) {
     event.preventDefault();
-    
     $('input[type=submit]').prop('disabled', true);
     var error = false;
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    
+
     if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
@@ -28,12 +25,14 @@ $(document).ready(function() {
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
+
     // Get the token from the response:
     var token = response.id;
+
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+
     // Submit the form:
     f.get(0).submit(); 
   }
-  
 });
