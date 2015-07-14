798b67ff82589bd797060c5c3183bc95567389bd
Yuri Ramocan <yramocan@umich.edu>
2015-07-14 04:30:55 +0000

    Changed new action in profiles controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     def show
         @user = User.find( params[:id] )
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
 end
